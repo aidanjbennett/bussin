43e0049c8ec69dd1a0c0ef8dcee27d8aa19fc443
Aidan Bennett <aidanbennett1103@gmail.com>
2022-02-18 15:16:31 +0000

    Change css placement

--- components/Home.vue
+++ components/Home.vue
@@ -15,16 +15,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-* {
-  margin: 0;
-  background-color: #2c3e50;
-  color: #ffff;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-
 h1 {
   margin-top: 10px;
 }
--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,15 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style>
+* {
+  margin: 0;
+  background-color: #2c3e50;
+  color: #ffff;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
